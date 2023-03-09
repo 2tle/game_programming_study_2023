df5aeaeb9c3dd2d3ee0a7d6efb9c1185985667e6
Hyunjun Yang <iam@2tle.io>
2023-03-09 14:17:15 +0900

    Study: 0309 fin.

--- transform/Assets/Move.cs
+++ transform/Assets/Move.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    public static float speed = 0.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,7 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         if(Input.GetKeyDown(KeyCode.RightArrow))
         {
             Debug.Log("오른쪽 키 눌림");
@@ -24,6 +27,63 @@ public class Move : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.RightArrow))
         {
             Debug.Log("오른쪽 키 눌렸음");
+        }*/
+
+        /*if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector3 vec = new Vector3(0.1f, 0.0f, 0.0f);
+            transform.Translate(vec);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 vec = new Vector3(-0.1f, 0.0f, 0.0f);
+            transform.Translate(vec);
+        }
+        if(Input.GetKey(KeyCode.UpArrow)) {
+            Vector3 vec = new Vector3(0.0f, 0.1f, 0.0f);
+            transform.Translate(vec);
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            Vector3 vec = new Vector3(0.0f, -0.1f, 0.0f);
+            transform.Translate(vec);
+        }*/
+
+        Vector3 vec = new Vector3(speed * Input.GetAxis("Horizontal"), speed * Input.GetAxis("Vertical"), 0.0f);
+        transform.Translate(vec);
+
+        /*if(Input.GetAxis("Horizontal") < 0)
+        {
+            if(transform.localScale.x > 0)
+            {
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            }
+        } 
+        else if(Input.GetAxis("Horizontal") > 0)
+        {
+            if(transform.localScale.x < 0)
+            {
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            }
+        }*/
+
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            if(transform.localScale.x > 0)
+            {
+                Vector3 characterScale = transform.localScale;
+                characterScale.x *= -1;
+                transform.localScale = characterScale;
+            }
+        }
+        else if(Input.GetAxis("Horizontal") > 0)
+        {
+            if (transform.localScale.x < 0)
+            {
+                Vector3 characterScale = transform.localScale;
+                characterScale.x *= -1;
+                transform.localScale = characterScale;
+            }
         }
 
     }
